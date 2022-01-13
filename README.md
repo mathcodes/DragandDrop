ecf776feb32d898e64293968f25a2b8054e6a1e1
Jon Christie <jonpchristie@gmail.com>
2022-01-12 21:54:23 -0500

    transfer from old repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Drag and Drop Jazz
+
+Simple but effective drag and drop like with order checker for many use cases, namely studying / TechED implementations.
+
+<img src="./jorder.png" width="50%"/>
+<img src="./jorder1.png" width="50%"/>
+<img src="./jorder2.png" width="50%"/>
+<img src="./jorder3.png" width="50%"/>
---
+++ script.js
@@ -0,0 +1,115 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const jazzMusicians = [
+  'Pianist Scott Joplin publishes his first two rags.', //1895
+  'The word "jazz" first appears in print.', // 1913
+  'Dizzy Gillespie and Thelonious Monk are born.', //1917
+  'Coleman Hawkins plays alongside Louis Armstrong in the Fletcher Henderson\'s Orchestra.', //1924
+  'John Coltrane and Miles Davis are born.', //1926
+  'Pianist William "Count" Basie makes his first recordings.', //1922
+  'RCA demonstrates the first 33 1/3 rmp long-playing disc.' //1931
+];
+
+// Store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+  [...jazzMusicians]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
+
+      listItem.setAttribute('data-index', index);
+
+      listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
+
+      listItems.push(listItem);
+
+      draggable_list.appendChild(listItem);
+    });
+
+  addEventListeners();
+}
+
+function dragStart() {
+  // console.log('Event: ', 'dragstart');
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+  // console.log('Event: ', 'dragenter');
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  // console.log('Event: ', 'dragleave');
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  // console.log('Event: ', 'dragover');
+  e.preventDefault();
+}
+
+function dragDrop() {
+  // console.log('Event: ', 'drop');
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector('.draggable').innerText.trim();
+
+    if (personName !== jazzMusicians[index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
+}
+
+check.addEventListener('click', checkOrder);
